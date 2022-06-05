b952142df1004211a598d78f8adef695e2c5273e
Pierre-Luc Perron <pierre-luc.perron@protonmail.com>
2022-06-05 13:00:26 -0400

    chore: new gh:TezRomacH/python-package-template

---
+++ pixelart/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""PixelArt is a prototype meant to teach myself desktop UI development"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
